cec5e5d8bea7237f32d945854f20b7b05c793c0f
chrisjune <chrisjune2701@gmail.com>
2019-12-05 19:03:09 +0900

    'refactor'

--- BOARDCOVER/BOARDCOVER_chrisjune.py
+++ BOARDCOVER/BOARDCOVER_chrisjune.py
@@ -1,48 +1,46 @@
 # BOARDCOVER
-cards = [
-    [(0, 0), (1, 0), (0, 1)],
-    [(0, 0), (1, 0), (1, 1)],
-    [(0, 0), (0, 1), (1, 1)],
-    [(0, 0), (1, 0), (1,-1)]
-]
+cards = [[(0, 0), (1, 0), (0, 1)], [(0, 0), (1, 0), (1, 1)], [(0, 0), (0, 1), (1, 1)], [(0, 0), (1, 0), (1, -1)]]
 board = []
+
 
 def count_board(blank_count):
     global count
-    first_free = (-1,-1)
 
     if blank_count % 3 != 0:
         return
 
     # find first index
+    first_free = (-1, -1)
     for row in range(len(board)):
         for col in range(len(board[0])):
             if board[row][col] == 0:
-                first_free = (row,col)
+                first_free = (row, col)
                 break
-        if first_free!=(-1,-1):
+        if first_free != (-1, -1):
             break
 
     if first_free == (-1, -1):
-        count+=1
+        count += 1
         return
 
+    # put card on board
     for card in cards:
         if covered(first_free, card, 1):
-            count_board(blank_count-3)
+            count_board(blank_count - 3)
         covered(first_free, card, -1)
 
-def covered(location, cards, action):
+
+def covered(location, card, action):
     result = True
-    for card in cards:
-        row = location[0]+card[0]
-        col = location[1]+card[1]
-        if row<0 or row>=len(board) or col<0 or col>=len(board[0]):
+    for ele in card:
+        row = location[0] + ele[0]
+        col = location[1] + ele[1]
+        if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]):
             result = False
             continue
         if board[row][col] == 1:
             result = False
-        board[row][col]+=action
+        board[row][col] += action
     return result
 
 
