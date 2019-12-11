18fc27e7a67146cc81c843f4e163e0f0a7a605db
JunyeongChoi <chrisjune@29cm.co.kr>
2019-12-12 02:40:26 +0900

    쿼드트리 문제해결(Iterator로 변경)

--- QUADTREE/QUADTREE_chrisjune.py
+++ QUADTREE/QUADTREE_chrisjune.py
@@ -4,55 +4,54 @@
 # x12x12344 -> xx3412412
 # 가장 작은 형태인 x1234를 뒤집어서 반환하게한다
 
-string = []
+
+string = None
+recursived = False
 
 
 def flip():
-    # 첫번째 요소를 검사
-    first = string.pop(0)
-    swap = []
-    # x 가 아니면 그대로 반환
+    # 재귀로 시작한 것인지 판단하기 위한 변수
+    global recursived
+    if recursived:
+        first = 'x'
+        recursived = False
+    else:
+        # 재귀로 시작한게 아니라면 입력 문자열에서 첫번째를 읽는다
+        first = next(string)
+
+    # 만약 첫 문자가 x로 시작하지 않으면 볼 필요없이 그대로반환
     if first != 'x':
         return first
 
-    # x로 시작하면 쿼드가 필요하므로 반복문으로 요소를 삽입한다
+    # 여기서부턴 x1234의 형태가 된다
+    # x도 결과문자에 포함해야하기 때문에 append
+    swap = []
     swap.append(first)
-    for i in range(0, len(string)):
-        # 어디까지 부분인지 모르기 때문에 swap에 넣은 개수가 x1234의 형태로 차게되면 반복을 끝내고 call stack을 빠져나가도록 한다
-        if len(swap) == 5:
-            break
+    try:
+        while True:
+            # 입력받은 문자열이 얼마나 긴지 모르기 때문에 판단여부를 swap할 대상리스트가
+            # 총 [x,1,2,3,4] 5개가 되면 끝낸다
+            if len(swap) == 5:
+                break
 
-        # 만약 1234라고 생각한 요소에서 x가 다시 등장하면 부분문제가 되므로 재귀를 호출한다
-        target = string.pop(0)
-        if target == 'x':
-            # 재귀 함수로 들어갈때 1234가 아닌 x1234로 형태로 반환받아야하기 때문에 x를 다시 붙여서 호출한다
-            string.insert(0, target)
-            # 재귀 결과또한 1234중 하나의 요소에 해당하므로 swap에 추가해준다
-            swap.append(flip())
-        else:
-            # 1234에서 숫자에 해당하므로 swap에 추가
-            swap.append(target)
-    # 모든 요소가 다 찼기 때문에 자리를 바꿔준다
+            # 여기선 x 이후의 문자가 되는데
+            # 다시 x가 등장하면 재귀를 호출하여 부분문제를 풀도록하고
+            # 숫자가 등장하면 x,1,2,3,4의 문자열이기 때문에 swap에 모두 넣어준다
+            target = next(string)
+            if target == 'x':
+                recursived = True
+                swap.append(flip())
+            else:
+                swap.append(target)
+    except StopIteration:
+        pass
+
+    # x1234 -> x3412로 되도록 순서변경해준다
     swap[1], swap[2], swap[3], swap[4] = swap[3], swap[4], swap[1], swap[2]
-    return swap
-
-
-result = []
-
-
-# Nested한 리스트를 flat하게 변경해준다
-# [[[x,1,2,3,4],2,3,4],2,3,4] -> [x,1,2,3,4,2,3,4,2,3,4]
-def convert_to_list(li):
-    for i in li:
-        if isinstance(i, list):
-            convert_to_list(i)
-        else:
-            result.append(i)
+    return ''.join(swap)
 
 
 if __name__ == '__main__':
     for case in range(int(input())):
-        string = list(input())
-        convert_to_list(flip())
-        print(''.join(result))
-        result = []
+        string = iter(input())
+        print(flip())
