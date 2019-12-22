275fde2f9921557f98d9a23d54008c5b95d35086
EunwooCho <jonnung@gmail.com>
2019-12-22 11:58:45 +0900

    [FENCE] 이미 계산된 펜스는 계산하지 않도록 체크하는 로직 추가

--- FENCE/fence_jonnung.py
+++ FENCE/fence_jonnung.py
@@ -1,18 +1,32 @@
+from collections import defaultdict
+
+
 def calculate_rectangle_size(width, boards):
     max_height = 0
+    same_heights_indies = defaultdict(list)
 
     for i, v in enumerate(boards):
         height = v
 
+        if v == 0:
+            continue
+
+        if i in same_heights_indies[v]:
+            continue
+
         for j in range(i+1, width):
             if v <= boards[j]:
                 height += v
+                if v == boards[j]:
+                    same_heights_indies[v].append(j)
             else:
                 break
 
         for k in range(i-1, -1, -1):
             if v <= boards[k]:
                 height += v
+                if v == boards[k]:
+                    same_heights_indies[v].append(k)
             else:
                 break
 
--- tests/FENCE/test_fence_jonnung.py
+++ tests/FENCE/test_fence_jonnung.py
@@ -9,6 +9,10 @@ def test__현재_위치_양옆으로_현재_높이보다_작을때까지_너비�
     fence2 = [7, 1, 5, 9, 6, 7, 3]
     rectangle_size2 = calculate_rectangle_size(len(fence2), fence2)
 
+    fence3 = [1, 8, 2, 2]
+    rectangle_size3 = calculate_rectangle_size(len(fence3), fence3)
+
     # assert
     assert rectangle_size1 == 16
     assert rectangle_size2 == 20
+    assert rectangle_size3 == 8
