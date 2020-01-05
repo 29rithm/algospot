600da8a728135376ec6ec9a3313bb6820c9c6cef
yoonnoon <jung.5h.yoon@gmail.com>
2020-01-05 20:01:06 +0900

    TrianglePath (옵션문제) solution

--- LIS/lis_yoonnoon.py
+++ LIS/lis_yoonnoon.py
@@ -117,3 +117,64 @@ class Test(unittest.TestCase):
         self.assertEqual(LISv1(n, data).solution(), 5)
         self.assertEqual(LISv2(n, data).solution(), 5)
         self.assertEqual(LISv3(n, data).solution(), 5)
+
+
+class TrianglePath:
+
+    def __init__(self, n, data):
+        self.n = n
+        self.data = data
+        self.memo = dict()
+
+    def solution(self):
+        for row in range(self.n):
+            for idx in range(len(self.data[row])):
+                upper = self.memo[(row-1, idx)] + self.data[row][idx] if row > 0 and idx < len(self.data[row-1]) else self.data[row][idx]
+                lupper = self.memo[(row-1, idx-1)] + self.data[row][idx] if row > 0 and idx > 0 else self.data[row][idx]
+
+                self.memo[(row, idx)] = max(upper, lupper)
+
+        return max(self.memo.values())
+
+
+def main_triangle_path():
+    tc = input()
+
+    for i in range(int(tc)):
+        n = int(input())
+        data = []
+        for j in range(n):
+            data.append([int(v) for v in input().split(' ')])
+        print(TrianglePath(n, data).solution())
+
+
+if __name__ == '__main__':
+    main_triangle_path()
+
+
+class TestTrianglePath(unittest.TestCase):
+
+    def test_main(self):
+        main_triangle_path()
+
+    def test_case1(self):
+        n = 5
+        data = [
+            [6],
+            [1, 2],
+            [3, 7, 4],
+            [9, 4, 1, 7],
+            [2, 7, 5, 9, 4]
+        ]
+        self.assertEqual(TrianglePath(n, data).solution(), 28)
+
+    def test_case2(self):
+        n = 5
+        data = [
+            [1],
+            [2, 4],
+            [8, 16, 8],
+            [32, 64, 32, 64],
+            [128, 256, 128, 256, 128]
+        ]
+        self.assertEqual(TrianglePath(n, data).solution(), 341)
