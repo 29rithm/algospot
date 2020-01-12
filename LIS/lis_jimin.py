9f4b22648b991ae1672430623326041e1fdb8c2d
lee.jimin <lee.jimin@29cm.co.kr>
2020-01-12 18:56:08 +0900

    leetcode 포맷으로 맞춘 LIS

--- LIS/lis_jimin.py
+++ LIS/lis_jimin.py
@@ -1,31 +1,36 @@
-def is_increasing_subsequence(sequence_data):
-    for index in range(len(sequence_data)):
-        if index == 0:
-            continue
+class Solution:
+    def __init__(self):
+        self.max_lis_cnt = 0
 
-        if sequence_data[index] < sequence_data[index-1]:
-            return False
+    def lengthOfLIS(self, nums):
+        self._search_lis(nums)
+        return self.max_lis_cnt
 
-    return True
+    def _search_lis(self, sequence_data):
+        if len(sequence_data) == 1 or self._is_increasing_subsequence(sequence_data):
+            candiate_cnt = len(sequence_data)
 
+            if candiate_cnt > self.max_lis_cnt:
+                self.max_lis_cnt = candiate_cnt
 
-def get_lis(sequence_data):
-    global max_lis_cnt
+            return sequence_data
 
-    if len(sequence_data) == 1 or is_increasing_subsequence(sequence_data):
-        candiate_cnt = len(sequence_data)
-
-        if candiate_cnt > max_lis_cnt:
-            max_lis_cnt = candiate_cnt
+        for index, number in enumerate(sequence_data):
+            sequence_data.pop(index)
+            self._search_lis(sequence_data)
+            sequence_data.insert(index, number)
 
         return sequence_data
 
-    for index, number in enumerate(sequence_data):
-        sequence_data.pop(index)
-        get_lis(sequence_data)
-        sequence_data.insert(index, number)
+    def _is_increasing_subsequence(self, sequence_data):
+        for index in range(len(sequence_data)):
+            if index == 0:
+                continue
 
-    return sequence_data
+            if sequence_data[index] < sequence_data[index-1]:
+                return False
+
+        return True
 
 if __name__ == '__main__':
     max_lis_cnt = 0
@@ -34,29 +39,13 @@ if __name__ == '__main__':
     # for case in range(int(input())):
     #     subsequence_cnt = int(input())
     #     subsequence = input()
-    #     print(get_lis(subsequence.split(' ')))
+    #     result = Solution().lengthOfLIS(subsequence.split(' '))
+    #     print(print)
 
-    # max_lis_cnt = 0
-    # get_lis([2, 1])
-    # assert max_lis_cnt == 1
-    #
-    # max_lis_cnt = 0
-    # get_lis([2, 1, 3])
-    # assert max_lis_cnt == 2
-    #
-    # max_lis_cnt = 0
-    # get_lis([1, 2, 3, 4])
-    # assert max_lis_cnt == 4
-    #
-    # max_lis_cnt = 0
-    # get_lis([5, 4, 3, 2, 1, 6, 7, 8])
-    # assert max_lis_cnt == 4
-    #
-    #
-    # max_lis_cnt = 0
-    # get_lis([5, 6, 7, 8, 1, 2, 3, 4])
-    # assert max_lis_cnt == 4
-
-    max_lis_cnt = 0
-    get_lis([10, 9, 2, 5, 3, 7, 101, 18])
-    assert max_lis_cnt == 4
+    assert Solution().lengthOfLIS([2, 1]) == 1
+    assert Solution().lengthOfLIS([2, 2]) == 1
+    assert Solution().lengthOfLIS([2, 1, 3]) == 2
+    assert Solution().lengthOfLIS([1, 2, 3, 4]) == 4
+    assert Solution().lengthOfLIS([5, 4, 3, 2, 1, 6, 7, 8]) == 4
+    assert Solution().lengthOfLIS([5, 6, 7, 8, 1, 2, 3, 4]) == 4
+    assert Solution().lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]) == 4
