15b4c3df13cbd0bf10be719f81ab17fe0ba5c8eb
chrisjune <chrisjune@gmail.com>
2020-02-02 10:40:30 +0900

    brute force and not cached

--- LONGEST_PALINDROMIC_SUBSTRING/longest_palindromic_substring_chrisjune.py
+++ LONGEST_PALINDROMIC_SUBSTRING/longest_palindromic_substring_chrisjune.py
@@ -1,18 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        mid = len(s)//2
-        left = mid
-        right = mid
-        for i in range(1, len(s)-mid):
-            if s[mid-i] != s[mid+i]:
-                break
-            left = mid - i
-            right = mid + i
-        palin = s[left:right+1]
+        max_result = ''
+        for i in range(len(s)):
+            max_result = max(max_result, self.calculate(s, i, i + 1), self.calculate(s, i,i), key=lambda x:len(x))
+        return max_result
 
-        maxstr = max(palin)
-        return palin
+    def calculate(self, s, left, right):
+        l, r = 0, 0
+        while 0 <= left and right <=len(s) - 1:
+            if s[left] == s[right]:
+                l, r = left, right
+                left -= 1
+                right += 1
+                continue
+            else:
+                break
+        return s[l: r+1]
+
+
 
 if __name__ == '__main__':
-    print(Solution().longestPalindrome('racecar'))
+    # assert Solution().longestPalindrome('aba') == 'aba'
+    assert Solution().longestPalindrome('xaba') == 'aba'
+    assert Solution().longestPalindrome('xxaba') == 'aba'
+    assert Solution().longestPalindrome('abax') == 'aba'
+    assert Solution().longestPalindrome('abaxx') == 'aba'
+    assert Solution().longestPalindrome('racecar') == 'racecar'
     assert Solution().longestPalindrome('cbbd') == 'bb'
